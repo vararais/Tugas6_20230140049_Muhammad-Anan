a56b575413e7bb98246b6e8494094bed4b095b78
vara <160727715+vararais@users.noreply.github.com>
2025-04-25 14:20:31 +0700

    Add files via upload

---
+++ jsweb.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("ini video Kereta Api");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini Audio Toto");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar Simpson");
+});
